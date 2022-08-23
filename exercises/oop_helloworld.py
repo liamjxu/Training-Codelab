0344093210283912beda9516a4552402a81113fe
Liam Xu <liamxu24@outlook.com>
2022-08-22 23:24:53 -0500

    1.3

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,4 +1,13 @@
 # create your class here
+class Car():
+    def __init__(self, model: str, speed: float):
+        self.model = model
+        self.speed = speed
+    def getModel(self):
+        return self.model
+    def setSpeed(self, val: float):
+        self.speed = val
+
 
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
