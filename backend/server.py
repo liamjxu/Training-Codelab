8cfe0c05a2d457a078592d9b0249e10a7537a077
Liam Xu <liamxu24@outlook.com>
2022-08-22 23:29:17 -0500

    3.1

--- backend/server.py
+++ backend/server.py
@@ -10,6 +10,7 @@ from custom_util import get_live_bitcoin_price, convert_date_to_text
 from database_connection import DatabaseConnection
 
 # TODO (3.1): define FastAPI app
+app = FastAPI()
 
 # TODO (5.4.1): define database connection
 
@@ -21,7 +22,10 @@ from database_connection import DatabaseConnection
 """
 a index function to test if server is running
 """
-
+@app.get('/')
+async def root():
+    content = {'message': "Hello World! This is a bitcoin monitoring service!"}
+    return json.dumps(content)
 
 # TODO (5.4.2)
 """
