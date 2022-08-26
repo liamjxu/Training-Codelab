9e64c3c82965b2594f56fbfa4f8ed26f1d470b2a
Liam Xu <liamxu24@outlook.com>
2022-08-26 18:34:59 -0500

    8

--- exercises/JavaScript/program.js
+++ exercises/JavaScript/program.js
@@ -1,10 +1,17 @@
 var body;
 
 // implement onload function
-
+window.onload = function () {
+    body = document.querySelector("body");
+}
 
 // implement changeColorBlue
+window.changeColorBlue = function () {
+    body.style.backgroundColor = "#13294b"
+}
 
 
-// implement changeColorPurple
-
+// implement changeColorOrange
+window.changeColorOrange = function () {
+    body.style.backgroundColor = "#ff5f05"
+}
