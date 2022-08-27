e81f9b2574b49542ca2f23a46210004490ccec2a
Liam Xu <liamxu24@outlook.com>
2022-08-26 19:05:02 -0500

    9.2

--- exercises/frontend/src/Components/TestButton.js
+++ exercises/frontend/src/Components/TestButton.js
@@ -14,13 +14,11 @@ function TestButton ({chosen,changeChosen}) {
   return (
     <div className={styles.bodyContainer}>
         <button onClick={()=>changeChosen("Button 1")}
-          /* uncomment for 9.2 className={chosen === 'Button 1' ?
-          styles.currencyButtonActive : styles.currencyButtonDefault} */>
+          className={chosen === 'Button 1' ? styles.currencyButtonActive : styles.currencyButtonDefault}>
           Button1
         </button>
         <button onClick={()=>changeChosen("Button 2")}
-          /* uncomment for 9.2 className={chosen === 'Button 2' ?
-          styles.currencyButtonActive : styles.currencyButtonDefault} */>
+          className={chosen === 'Button 2' ? styles.currencyButtonActive : styles.currencyButtonDefault}>
           Button2
         </button>
     </div>         
