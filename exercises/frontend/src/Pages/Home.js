3f03208501edd4b641bb348517d4cc417da94766
Liam Xu <liamxu24@outlook.com>
2022-08-26 19:08:20 -0500

    9.3

--- exercises/frontend/src/Pages/Home.js
+++ exercises/frontend/src/Pages/Home.js
@@ -5,16 +5,17 @@ import styles from "./Home.module.css"
 function Home () {
   // set variables [chosen, setChosen] using hooks (useState) to "button 1"
   const [chosen, setChosen] = useState("Button 1")
-
+  
   //  excercise 9.3
   // set variables [message, setMessage] using hooks (useState) to "you choose 1"
+  const [message, setMessage] = useState("You Choose 1")
   
 
   //  excercise 9.3 update message when chosen change ( set dependecy to chosen) 
-  /* uncomment for excercise 9.3
   useEffect(() =>{
-  },[])
-  */
+    setMessage(chosen === "Button 1" ? "You Choose 1" : "You Choose 2")
+  },[chosen])
+  
 
   // handle currency button onclick
   const changeChosen = (clickedButton) =>{
@@ -27,7 +28,7 @@ function Home () {
           {chosen}
            {/* exercise 9.1 insert TestButton pass chosen and changeChosen ( pass by chosen={chosen})*/}
            <TestButton chosen={chosen} changeChosen={changeChosen}/>
-          {/* uncomment for exercise 9.3 {message} */}
+          {message}
         </div>
       </div>
   );
